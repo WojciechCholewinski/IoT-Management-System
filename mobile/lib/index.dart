dfe06e7a032955b9f88aeca3f379aa8a69c849b4
Wojciech Cholewiński <wojciech.cholewinski@interia.pl>
2024-08-14 13:59:32 +0200

    refactor(mobile-ui): reorganize Settings and Dashboard pages structure
    - Renamed `dashboard` folder to `dashboard_page` and `settings` folder to `settings_page` for consistent naming.
    - Moved `DashboardModel` and `DashboardWidget` to the `dashboard_page` folder.
    - Moved `SettingsModel` and `SettingsWidget` to the `settings_page` folder.
    - Updated references in `index.dart` to reflect the new folder structure.

--- mobile/lib/index.dart
+++ mobile/lib/index.dart
@@ -1,5 +1,5 @@
 // Export pages
 export '/pages/login_page/login_page_widget.dart' show LoginPageWidget;
-export '/pages/dashboard/dashboard_widget.dart' show DashboardWidget;
-export '/pages/settings/settings_widget.dart' show SettingsWidget;
+export 'pages/dashboard_page/dashboard_widget.dart' show DashboardWidget;
+export 'pages/settings_page/settings_widget.dart' show SettingsWidget;
 export '/pages/register_page/register_page_widget.dart' show RegisterPageWidget;
