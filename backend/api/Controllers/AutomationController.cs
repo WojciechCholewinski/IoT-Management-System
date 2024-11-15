450686f0781ffed882e457d784561fb7467378f9
Wojciech Cholewiński <wojciech.cholewinski@interia.pl>
2024-11-15 11:22:30 +0100

    docs(backend-automation): add Swagger documentation for AutomationController endpoints
    - Added XML comments to all endpoints in 'AutomationController' to generate detailed Swagger documentation.
    - Included descriptions of endpoint functionality, and expected status codes.

--- backend/api/Controllers/AutomationController.cs
+++ backend/api/Controllers/AutomationController.cs
@@ -1,5 +1,6 @@
 ﻿using api.Models;
 using api.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace api.Controllers
@@ -15,37 +16,83 @@ namespace api.Controllers
             _automationService = automationService;
         }
 
+        /// <summary>
+        /// Retrieves all automations available in the system.
+        /// </summary>
+        /// <response code="200">(OK)           Returned when the automations data has been successfully retrieved.</response>
+        /// <response code="401">(Unauthorized) Returned when the user is not authenticated.</response>
+        [Authorize]
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         public ActionResult<IEnumerable<AutomationDetailDto>> GetAll()
         {
             var result = _automationService.GetAll();
-
             return Ok(result);
         }
 
+        /// <summary>
+        /// Retrieves the details of an automation by its ID.
+        /// </summary>
+        /// <response code="200">(OK)           Returned when the automation details are successfully retrieved. </response>
+        /// <response code="401">(Unauthorized) Returned when the user is not authenticated.</response>
+        /// <response code="404">(NotFound)     Returned when the automation with the given ID was not found.</response>
+        [Authorize]
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public ActionResult<AutomationDetailDto> GetById(int id)
         {
             var automationDto = _automationService.GetById(id);
             return Ok(automationDto);
         }
 
+        /// <summary>
+        /// Updates the details of a specific automation, excluding the management of associated devices.
+        /// </summary>
+        /// <response code="200">(OK)           Returned when the automation details are successfully updated. </response>
+        /// <response code="401">(Unauthorized) Returned when the user is not authenticated.</response>
+        /// <response code="404">(NotFound)     Returned when the automation with the given ID was not found.</response>
+        [Authorize]
         [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public IActionResult Update([FromRoute] int id, [FromBody] AutomationUpdateDto dto)
         {
             _automationService.Update(id, dto);
-
             return Ok(new { message = "Automation state updated successfully" });
         }
 
+        /// <summary>
+        /// Adds devices to the lists of devices to turn on and turn off for an automation.
+        /// </summary>
+        /// <response code="200">(OK)           Returned when the device lists are successfully updated. </response>
+        /// <response code="401">(Unauthorized) Returned when the user is not authenticated.</response>
+        /// <response code="404">(NotFound)     Returned when either the automation or one or more devices with the given IDs are not found in the database. </response>
+        [Authorize]
         [HttpPost("{Automationid}/devices")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public IActionResult AddDevices([FromRoute] int Automationid, [FromBody] AddDevicesToAutomationDto dto)
         {
             _automationService.AddDevices(Automationid, dto);
             return Ok();
         }
 
+        /// <summary>
+        /// Removes devices from the lists of devices to turn on and turn off for an automation.
+        /// </summary>
+        /// <response code="204">(NoContent)    Returned when the device lists are successfully updated.</response>
+        /// <response code="401">(Unauthorized) Returned when the user is not authenticated.</response>
+        /// <response code="404">(NotFound)     Returned when either the automation or one or more devices with the given IDs are not found in the database. </response>
+        [Authorize]
         [HttpDelete("{Automationid}/devices")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public ActionResult RemoveDevices([FromRoute] int Automationid, [FromBody] RemoveDevicesFromAutomationDto dto)
         {
             _automationService.RemoveDevices(Automationid, dto);
